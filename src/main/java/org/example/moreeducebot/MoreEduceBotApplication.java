0c9acd743ce500ec072409b3091c1222fb9da135
Bunyodbek Nurmamatov <bunyodbeknurmamatov@gmail.com>
2025-05-20 09:55:28 +0500

    add project

---
+++ src/main/java/org/example/moreeducebot/MoreEduceBotApplication.java
@@ -0,0 +1,13 @@
+package org.example.moreeducebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoreEduceBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoreEduceBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/moreeducebot/MoreEduceBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.moreeducebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoreEduceBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
