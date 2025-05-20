053986e2cb7853fed174110bfdab6e409e7a65f0
Bunyodbek Nurmamatov <bunyodbeknurmamatov@gmail.com>
2025-05-20 10:16:38 +0500

    add package

---
+++ src/main/java/org/example/moreeducebot/model/MyBot.java
@@ -0,0 +1,4 @@
+package org.example.moreeducebot.model;
+
+public class MyBot {
+}
---
+++ src/main/java/org/example/moreeducebot/repository/UsersRepo.java
@@ -0,0 +1,7 @@
+package org.example.moreeducebot.repository;
+
+import org.example.moreeducebot.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepo extends JpaRepository<Users , Long> {
+}
